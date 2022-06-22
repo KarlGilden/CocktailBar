98951af526296f572236886b1fefafc315eb8f4a
KarlGilden <karl.gildenhuys@gmail.com>
2022-06-22 19:21:41 +1200

    spell fix and font size change

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className=' w-[100%] h-[400px] bg-gradient-to-r from-yellow-500 to-pink-500 px-20 py-10 font-poppins'>
-        <h1 className='text-white text-6xl font-bold'>The best cocktail recipies to liven up your next event</h1>
+        <h1 className='text-white text-3xl sm:text-6xl font-bold'>The best cocktail recipes to liven up your next event</h1>
     </div>
   )
 }
