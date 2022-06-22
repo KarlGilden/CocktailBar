0e084243856272667b9f828ede51251eec0df4b2
KarlGilden <karl.gildenhuys@gmail.com>
2022-06-22 22:49:56 +1200

    change footer UI

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='p-6 flex items-center'>        
-        <a href='https://dryicons.com/free-icons/cocktail'> Icon by Dryicons </a>
+    <div className='w-[100%] h-[100px] p-6 flex items-center bg-zinc-800'>        
+        <a className='text-white' href='https://dryicons.com/free-icons/cocktail'> Icon by Dryicons </a>
     </div>
   )
 }
