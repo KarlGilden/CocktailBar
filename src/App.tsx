b6aea9441aadef261d31306d73e7d42adc048c61
KarlGilden <karl.gildenhuys@gmail.com>
2022-06-29 14:56:43 +1200

    add drink page and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Categories from './components/Categories/Categories';
+import DrinkPage from './components/DrinkPage/DrinkPage';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Home from './components/Home/Home';
@@ -13,6 +14,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/drink/:id" element={<DrinkPage/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
---
+++ src/components/DrinkPage/DrinkPage.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getDrinks } from '../../api/GetDrinks'
+
+const DrinkPage = () => {
+
+    let {id} = useParams<any>()
+
+    const [drink, setDrink] = useState<any>("")
+    useEffect(()=>{
+        getData()
+    })
+
+    const getData = async () => {
+        const data =  await getDrinks('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+id)
+        console.log(data)
+        setDrink(data.drinks[0])
+    
+    }
+
+  return (
+    <div className='w-full px-[5%] py-5 sm:flex'>
+
+        <img className='w-[100%] sm:w-[50%] md:w-[25%] sm:mr-5' src={drink?.strDrinkThumb} alt="" />
+
+        <div>
+            <h1 className='text-3xl'>{drink?.strDrink}</h1>
+            <br />
+            <p><i>{drink?.strMeasure1}</i> {drink?.strIngredient1}</p>
+            <p><i>{drink?.strMeasure2}</i> {drink?.strIngredient2}</p>
+            <p><i>{drink?.strMeasure3}</i> {drink?.strIngredient3}</p>
+            <p><i>{drink?.strMeasure4}</i> {drink?.strIngredient4}</p>
+            <p><i>{drink?.strMeasure5}</i> {drink?.strIngredient5}</p>
+            <p><i>{drink?.strMeasure6}</i> {drink?.strIngredient6}</p>
+            <p><i>{drink?.strMeasure7}</i> {drink?.strIngredient7}</p>
+            <br />
+            <p>{drink?.strInstructions}</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default DrinkPage
