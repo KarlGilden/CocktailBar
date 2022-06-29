f3c10d7ee940f6ba4eb10059fde30591043439be
KarlGilden <karl.gildenhuys@gmail.com>
2022-06-29 14:56:55 +1200

    update button UI

--- src/components/Buttons/HeroButton/HeroButton.tsx
+++ src/components/Buttons/HeroButton/HeroButton.tsx
@@ -7,7 +7,7 @@ interface IProps{
 
 const HeroButton = ({text, secondary}:IProps) => {
   return (
-    <button className={`p-6 z-1 text-2xl font-poppins ${secondary ? 'border-[3px] border-l-primary border-b-primary border-r-secondary border-t-secondary transition-all' : 'bg-gradient-to-r'} hover:bg-gradient-to-r transition-all from-secondary to-primary rounded-sm w-full h-[85px] first:mb-[25px] sm:first:mb-0 sm:mr-5 sm:w-[300px]`}>{text}</button>
+    <button className={`p-6 z-1 text-2xl font-poppins ${secondary ? 'border-[5px] border-l-primary border-b-primary border-r-secondary border-t-secondary transition-all' : 'bg-gradient-to-r'} hover:bg-gradient-to-r transition-all from-secondary to-primary rounded-sm w-full h-[85px] first:mb-[25px] sm:first:mb-0 sm:mr-5 sm:w-[300px]`}>{text}</button>
   )
 }
 
--- src/components/DrinkPreview/DrinkPreview.tsx
+++ src/components/DrinkPreview/DrinkPreview.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import MenuButton from '../Buttons/MenuButton/MenuButton'
 
 interface IProps{
@@ -6,12 +7,17 @@ interface IProps{
 }
 
 const DrinkPreview = ({drink}:IProps) => {
+  const navigate = useNavigate()
+
+  const toDrinkPage = () => {
+    navigate("/drink/"+drink.idDrink)
+  }
   return (
     <div className='w-[300px] h-[300px] relative group'>
             <div className='bg-black absolute w-full h-full'></div>
             <img className='w-full absolute top-0 left-0 group-hover:opacity-30' src={drink.strDrinkThumb} alt="Unavailable" />
             <div className='w-full h-full grid grid-cols-1 justify-items-center content-center absolute'>
-              <button onClick={()=>alert("hello")} className='hidden group-hover:block hover:bg-black hover:border-white hover:text-white transition w-[50%] h-[50px] text-white border-[2px] border-white'>See Recipe</button>
+              <button onClick={()=>toDrinkPage()} className='hidden group-hover:block hover:bg-black hover:text-white transition w-[50%] h-[50px] text-white border-[4px] border-l-primary border-b-primary border-r-secondary border-t-secondary'>See Recipe</button>
             </div>
     </div>
   )
